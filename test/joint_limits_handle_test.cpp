a9e1f0972adbce5af9486e0c2d53c13f60fcaed1
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2018-04-14 16:40:51 +0900

    first code commit

---
+++ test/joint_limits_handle_test.cpp
@@ -0,0 +1,71 @@
+#include <gtest/gtest.h>
+
+#include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/joint_state_interface.h>
+#include <joint_limits_interface/joint_limits.h>
+#include <joint_limits_interface/joint_limits_interface.h>
+
+#include "range_based_handle.h"
+
+template < class VelocitySaturationHandle > void test() {
+  // General joint handle
+  double pos(0.), vel(0.), eff(0.), cmd(0.);
+  hardware_interface::JointStateHandle state_handle("joint", &pos, &vel, &eff);
+  hardware_interface::JointHandle command_handle(state_handle, &cmd);
+
+  // Condition: velocity & acceleration limits are active
+  joint_limits_interface::JointLimits limits;
+  limits.has_velocity_limits = true;
+  limits.max_velocity = 5.;
+  limits.has_acceleration_limits = true;
+  limits.max_acceleration = 20.;
+
+  // Velocity command saturation handle to be tested
+  VelocitySaturationHandle saturation_handle(command_handle, limits);
+
+  //
+  // Case 1: velocity is greater than upper velocity limit
+  //     this is not a special case when external force acts the joint
+  //     (ex. collaborative robot touched by human).
+  //
+  const double vel_case1(10.);
+  vel = vel_case1;
+
+  // Velocity command saturation
+  cmd = 0.;
+  saturation_handle.enforceLimits(ros::Duration(0.1));
+  const double cmd_case1(cmd);
+
+  //
+  // Case 2: velocity is inverted
+  //
+  vel = -vel_case1;
+
+  // Velocity command saturation
+  cmd = 0.;
+  saturation_handle.enforceLimits(ros::Duration(0.1));
+  const double cmd_case2(cmd);
+
+  // Expectation: inverted saturation
+  EXPECT_TRUE(cmd_case1 == -cmd_case2);
+
+  ROS_INFO_STREAM("\n"
+                  << "Saturated command\n"
+                  << "    case 1: " << cmd_case1 << "\n"
+                  << "    case 2: " << cmd_case2);
+}
+
+// Test for current implementation
+TEST(VelocitySaturation, velocityJointSaturationHandle) {
+  test< joint_limits_interface::VelocityJointSaturationHandle >();
+}
+
+// Test for proposed range-based implementation
+TEST(VelocitySaturation, rangeBasedSaturation) {
+  test< range_based::VelocityJointSaturationHandle >();
+}
+
+int main(int argc, char *argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
---
+++ test/range_based_handle.h
@@ -0,0 +1,69 @@
+#ifndef RANGE_BASED_RANGE_BASED_HANDLE_H
+#define RANGE_BASED_RANGE_BASED_HANDLE_H
+
+#include <cmath>
+#include <limits>
+
+#include <hardware_interface/joint_command_interface.h>
+#include <joint_limits_interface/joint_limits.h>
+#include <ros/duration.h>
+
+#include <boost/algorithm/clamp.hpp>
+
+namespace range_based {
+
+// Helper class to deal with range bounds
+//   this would be more robust than current implementation
+//   and make enforceLimits() functions more readable
+class Range {
+public:
+  Range(const double bound0, const double bound1)
+      : min_(std::min(bound0, bound1)), max_(std::max(bound0, bound1)) {}
+  // initializers
+  static Range entire() {
+    return Range(-std::numeric_limits< double >::max(), std::numeric_limits< double >::max());
+  }
+  static Range positive() { return Range(0., std::numeric_limits< double >::max()); }
+  static Range negative() { return Range(-std::numeric_limits< double >::max(), 0.); }
+  // getters
+  double min() const { return min_; }
+  double max() const { return max_; }
+  // saturation
+  double clamp(const double val) { return boost::algorithm::clamp(val, min_, max_); }
+  Range clamp(const Range &range) { return Range(clamp(range.min_), clamp(range.max_)); }
+
+private:
+  double min_, max_;
+};
+
+class VelocityJointSaturationHandle {
+public:
+  VelocityJointSaturationHandle(const hardware_interface::JointHandle &jh,
+                                const joint_limits_interface::JointLimits &limits)
+      : jh_(jh), limits_(limits) {}
+
+  void enforceLimits(const ros::Duration &period) {
+    // Default: unlimited velocity bounds
+    Range vel_range(Range::entire());
+    // Overwrite bounds according to acc limits
+    if (limits_.has_acceleration_limits) {
+      const double vel(jh_.getVelocity());
+      const double delta_vel(limits_.max_acceleration * period.toSec());
+      vel_range = Range(vel - delta_vel, vel + delta_vel).clamp(vel_range);
+    }
+    // Overwrite bound according to vel limits
+    if (limits_.has_velocity_limits) {
+      vel_range = Range(-limits_.max_velocity, limits_.max_velocity).clamp(vel_range);
+    }
+    // Clamp the command with bounds
+    jh_.setCommand(vel_range.clamp(jh_.getCommand()));
+  }
+
+private:
+  hardware_interface::JointHandle jh_;
+  joint_limits_interface::JointLimits limits_;
+};
+
+} // namespace range_based
+
+#endif
